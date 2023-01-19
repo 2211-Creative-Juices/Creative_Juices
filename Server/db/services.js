eb42bb2e843551c644fc32b541857f6f99e3ea95
Philip Rau <pjrau3@gmail.com>
2023-01-19 14:09:53 -0600

    added services helper funcs

--- Server/db/services.js
+++ Server/db/services.js
@@ -27,4 +27,134 @@ async function createService({
   }
 }
 
-module.exports = { createService };
+async function getAllServices() {
+  try {
+    const { rows: service } = await client.query(`
+    SELECT *
+    FROM services
+    `);
+    console.log('These are our all our services :', service);
+    return service;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getServiceByUser({ username }) {
+  try {
+    const { rows: service } = await client.query(
+      `
+    SELECT *
+    FROM services
+    WHERE username = $1
+    `,
+      [username]
+    );
+    console.log('These are our services by username :', service);
+    return service;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getServiceById({ id }) {
+  try {
+    const { rows: service } = await client.query(
+      `
+    SELECT *
+    FROM services
+    WHERE id = $1
+    `,
+      [id]
+    );
+    console.log('These are our services by id:', service);
+    return service;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getServiceByDate({ date }) {
+  try {
+    const { rows: service } = await client.query(
+      `
+    SELECT *
+    FROM services
+    WHERE date = $1
+    `,
+      [date]
+    );
+    console.log('These are our services by date:', service);
+    return service;
+  } catch (error) {}
+}
+
+async function getServiceByName({ name }) {
+  try {
+    const { rows: service } = await client.query(
+      `
+    SELECT *
+    FROM services
+    WHERE name = $1
+    `,
+      [name]
+    );
+    console.log('These are our services by service name:', service);
+    return service;
+  } catch (error) {}
+}
+
+async function updateService({ id, ...fields }) {
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(', ');
+
+  try {
+    const {
+      rows: [service],
+    } = await client.query(
+      `
+    UPDATE services
+    SET ${setString}
+    WHERE id = ${id}
+    RETURNING *;
+    `,
+
+      Object.values(fields)
+    );
+    console.log('These are my update services: ', service);
+    return service;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function destroyService(id) {
+  try {
+    const {
+      rows: [service],
+    } = await client.query(
+      `
+    DELETE FROM services
+    WHERE id = $1
+    RETURNING *,
+    `,
+      [id]
+    );
+    console.log('These are my destroy services: ', service);
+    return service;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = {
+  createService,
+  getAllServices,
+  getServiceByUser,
+  getServiceById,
+  getServiceByName,
+  getServiceByDate,
+  updateService,
+  destroyService,
+};
