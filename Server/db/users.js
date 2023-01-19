50807427661125b1cb67c47e71c217517e2405e6
Ashley Hoath <ahoath87@gmail.com>
2023-01-19 09:41:46 -0700

    added hashed password

--- Server/db/users.js
+++ Server/db/users.js
@@ -1,7 +1,10 @@
 const client = require('./client');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 async function createUser({ name, username, password, zipcode, email }) {
   try {
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
     const {
       rows: [user],
     } = await client.query(
@@ -11,7 +14,7 @@ async function createUser({ name, username, password, zipcode, email }) {
         ON CONFLICT (username, email) DO NOTHING
         RETURNING id, name, username, zipcode, email
         `,
-      [name, username, password, zipcode, email]
+      [name, username, hashedPassword, zipcode, email]
     );
     return user;
   } catch (error) {
