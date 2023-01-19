9e05e02946a2a763a9ad03485dfa6d26aa2d7f42
Chelsea <angelofwarcraft@gmail.com>
2023-01-19 13:21:58 -0600

    user helper functions created

--- Server/db/users.js
+++ Server/db/users.js
@@ -22,4 +22,77 @@ async function createUser({ name, username, password, zipcode, email }) {
   }
 }
 
-module.exports = { createUser };
+async function getUserByUsername(username) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+    SELECT *
+    FROM users
+    WHERE username=$1`,
+      [username]
+    );
+
+    return user;
+  } catch (error) {
+    console.log('Error getting user by Username');
+    throw error;
+  }
+}
+
+async function getUser({ username, password }) {
+  try {
+    const user = await getUserByUsername(username);
+    if (!user) return;
+    const hashedPassword = user.password;
+    const passwordsMatch = await bcrypt.compare(password, hashedPassword);
+    if (!passwordsMatch) return;
+    delete user.password;
+
+    return user;
+  } catch (error) {
+    console.error('Error getting user');
+    throw error;
+  }
+}
+
+async function getUserById(id) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(`
+    SELECT id, username, zipcode, email, name
+    FROM users
+    WHERE id=${id}`);
+
+    return user;
+  } catch (error) {
+    console.error('Error getting user by ID');
+    throw error;
+  }
+}
+
+async function getUserByEmail(email) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(`
+    SELECT id, username, zipcode, email, name
+    FROM users
+    WHERE email=${email}`);
+
+    return user;
+  } catch (error) {
+    console.error('Error getting user by eMail');
+    throw error;
+  }
+}
+
+module.exports = {
+  createUser,
+  getUserByUsername,
+  getUser,
+  getUserById,
+  getUserByEmail,
+};
