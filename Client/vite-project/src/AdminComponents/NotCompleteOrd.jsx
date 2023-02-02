673f9fbdf2879af58cee94ca11bfe057a075030b
Megan Miller <meganmiller485@gmail.com>
2023-02-02 11:27:16 -0700

    mergin main

--- Client/vite-project/src/AdminComponents/NotCompleteOrd.jsx
+++ Client/vite-project/src/AdminComponents/NotCompleteOrd.jsx
@@ -15,26 +15,37 @@ const NotCompleteOrd = ({ orders }) => {
         {orders &&
           orders.map((order) => {
             if (!order.iscomplete) {
-              
               return (
-                <div key={order.id} className='order'>
+                <div
+                  key={order.id}
+                  className='order'
+                >
                   <h3>Incomplete Orders:</h3>
                   <p>Order Date: {order.orderdate}</p>
                   <p>Purchaser ID: {order.purchaserId}</p>
                   <p>Service ID: {order.serviceId}</p>
                   <p>Bundle Kit ID: {order.bundlekitId}</p>
                   <p>{order.iscomplete ? 'Complete!' : 'Not Complete'}</p>
-                  <button onClick={async () => {
-                  
-                    const ordComplete = order.iscomplete;
-                    console.log("ORDERIDIDIDIDODERDOEIRDO:", order.id);
-                    const updatedOrder= await updateOrder(user.token, order.id, order.orderdate, order.purchaserId, !ordComplete, order.incart, order.serviceId, order.bundlekitId);
-                    console.log("order statussssssssss: ", updatedOrder);
-                    console.log("isorder!!!!!compleeeete:", !ordComplete);
-                    console.log("IDIIDIDIDcompleeeete:", isOrderId);
-                  }
-
-                  } type={'submit'}>
+                  <button
+                    onClick={async () => {
+                      const ordComplete = order.iscomplete;
+                      console.log('ORDERIDIDIDIDODERDOEIRDO:', order.id);
+                      const updatedOrder = await updateOrder(
+                        user.token,
+                        order.id,
+                        order.orderdate,
+                        order.purchaserId,
+                        !ordComplete,
+                        order.incart,
+                        order.serviceId,
+                        order.bundlekitId
+                      );
+                      console.log('order statussssssssss: ', updatedOrder);
+                      console.log('isorder!!!!!compleeeete:', !ordComplete);
+                      console.log('IDIIDIDIDcompleeeete:', isOrderId);
+                    }}
+                    type={'submit'}
+                  >
                     Mark As Complete
                   </button>
                 </div>
