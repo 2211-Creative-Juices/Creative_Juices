a2ac26d4adc0ba907cf8ac1de0c78838a321a22e
Megan Miller <meganmiller485@gmail.com>
2023-02-03 13:39:17 -0700

    some stylin and new footer comp

--- Client/vite-project/src/Components/Contact.jsx
+++ Client/vite-project/src/Components/Contact.jsx
@@ -3,69 +3,77 @@ import { Form, Input, TextArea, Button } from 'semantic-ui-react';
 import emailjs from 'emailjs-com';
 import Swal from 'sweetalert2';
 
-const SERVICE_ID = "philip-rau-email";
-const TEMPLATE_ID = "fake-email-template-id";
-const USER_ID = "a_hjRYWHRArTH61UM";
-
+const SERVICE_ID = 'philip-rau-email';
+const TEMPLATE_ID = 'fake-email-template-id';
+const USER_ID = 'a_hjRYWHRArTH61UM';
 
 const ContactForm = () => {
-    const handleOnSubmit = (e) => {
-        e.preventDefault();
-        emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
-          .then((result) => {
-            console.log(result.text);
-            Swal.fire({
-              icon: 'success',
-              title: 'Message Sent Successfully'
-            })
-          }, (error) => {
-            console.log(error.text);
-            Swal.fire({
-              icon: 'error',
-              title: 'Ooops, something went wrong',
-              text: error.text,
-            })
-          });
-        e.target.reset()
-      };
-    
-  
-    return (
-      <div className='ContactForm'>
-        <Form onSubmit={handleOnSubmit} className='FormContactForm' >
-          <Form.Field
-            id='form-input-control-email'
-            control={Input}
-            label='Email'
-            name='user_email'
-            placeholder='Email…'
-            required
-            // icon='mail'
-            // iconPosition='left'
-          />
-          <Form.Field
-            id='form-input-control-last-name'
-            control={Input}
-            label='Name'
-            name='user_name'
-            placeholder='Name…'
-            required
-            // icon='user circle'
-            // iconPosition='left'
-          />
-          <Form.Field
-            id='form-textarea-control-opinion'
-            control={TextArea}
-            label='Message'
-            name='user_message'
-            placeholder='Message…'
-            required
-          />
-          <Button type='submit' color='green'>Submit</Button>
-        </Form>
-      </div>
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID).then(
+      (result) => {
+        console.log(result.text);
+        Swal.fire({
+          icon: 'success',
+          title: 'Message Sent Successfully',
+        });
+      },
+      (error) => {
+        console.log(error.text);
+        Swal.fire({
+          icon: 'error',
+          title: 'Ooops, something went wrong',
+          text: error.text,
+        });
+      }
     );
-  }
-  
-  export default ContactForm;
-  
+    e.target.reset();
+  };
+
+  return (
+    <div className='ContactForm-container'>
+      <Form
+        onSubmit={handleOnSubmit}
+        className='ContactForm'
+      >
+        <div>Contact Shelley:</div>
+        <Form.Field
+          id='form-input-control-email'
+          control={Input}
+          label='Email'
+          name='user_email'
+          placeholder='Email…'
+          required
+          // icon='mail'
+          // iconPosition='left'
+        />
+        <Form.Field
+          id='form-input-control-last-name'
+          control={Input}
+          label='Name'
+          name='user_name'
+          placeholder='Name…'
+          required
+          // icon='user circle'
+          // iconPosition='left'
+        />
+        <Form.Field
+          id='form-textarea-control-opinion'
+          control={TextArea}
+          label='Message'
+          name='user_message'
+          placeholder='Message…'
+          required
+        />
+        <Button
+          type='submit'
+          color='green'
+        >
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default ContactForm;
