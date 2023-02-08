47c3f7e32640a946ff084029dd0495f625a1feba
Ashley Hoath <ahoath87@gmail.com>
2023-02-08 09:00:32 -0700

    final styling

--- Client/vite-project/src/Components/Footer.jsx
+++ Client/vite-project/src/Components/Footer.jsx
@@ -3,8 +3,9 @@ import ContactForm from './Contact';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faSquareFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faTiktok } from '@fortawesome/free-brands-svg-icons';
+import shellcontact from '../assets/images/shellcontact.png';
 import CJlogonobckgrnd from '../assets/images/CJlogonobckgrnd.png';
-
 
 const Footer = () => {
   return (
@@ -12,10 +13,17 @@ const Footer = () => {
       <div id='footer-logo-container'>
         <img id='footer-logo' src={CJlogonobckgrnd}></img>
       </div>
+      <div id='shellimg-contact'>
+        <img id='shellimg' src={shellcontact}></img>
+        <p> Thank you for supporting my small business!</p>
+      </div>
       <div className='socials'>
         <div>
-          <a href='https://www.instagram.com/creativejuices.paint/?fbclid=IwAR1_TkmS9e-EwyQy98DVkQoBaHB21TzEQRe1wRTDB25ENU9tcapIWKZROc4'
-            target="_blank" rel="noopener noreferrer">
+          <a
+            href='https://www.instagram.com/creativejuices.paint/?fbclid=IwAR1_TkmS9e-EwyQy98DVkQoBaHB21TzEQRe1wRTDB25ENU9tcapIWKZROc4'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
             <FontAwesomeIcon
               className='instagram-social'
               icon={faInstagram}
@@ -25,11 +33,28 @@ const Footer = () => {
           </a>
         </div>
         <div>
-          <a href='https://www.facebook.com/creativejuices.paint'
-            target="_blank" rel="noopener noreferrer">
+          <a
+            href='https://www.facebook.com/creativejuices.paint'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
             <FontAwesomeIcon
               className='facebook-social'
               icon={faSquareFacebook}
+              size='3x'
+              color='black'
+            />
+          </a>
+        </div>
+        <div>
+          <a
+            href='https://www.tiktok.com/@creativejuices_paint?_t=8Zi0xluFD8U&_r=1'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FontAwesomeIcon
+              className='tiktok-social'
+              icon={faTiktok}
               size='3x'
               color='black'
             />
