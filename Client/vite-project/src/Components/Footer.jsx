f9c0c4228279897eec50aa979ddb24705cbd9f3b
Megan Miller <meganmiller485@gmail.com>
2023-02-09 12:58:33 -0700

    footer update

--- Client/vite-project/src/Components/Footer.jsx
+++ Client/vite-project/src/Components/Footer.jsx
@@ -10,59 +10,69 @@ import CJlogonobckgrnd from '../assets/images/CJlogonobckgrnd.png';
 const Footer = () => {
   return (
     <footer id='footer-container'>
-      <div id='footer-logo-container'>
-        <img id='footer-logo' src={CJlogonobckgrnd}></img>
-      </div>
-      <div id='shellimg-contact'>
-        <img id='shellimg' src={shellcontact}></img>
-        <p> Thank you for supporting my small business!</p>
-      </div>
-      <div className='socials'>
-        <div>
-          <a
-            href='https://www.instagram.com/creativejuices.paint/?fbclid=IwAR1_TkmS9e-EwyQy98DVkQoBaHB21TzEQRe1wRTDB25ENU9tcapIWKZROc4'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FontAwesomeIcon
-              className='instagram-social'
-              icon={faInstagram}
-              size='3x'
-              color='black'
-            />
-          </a>
+      <div id='logo_photo'>
+        <div id='footer-logo-container'>
+          <img
+            id='footer-logo'
+            src={CJlogonobckgrnd}
+          ></img>
         </div>
-        <div>
-          <a
-            href='https://www.facebook.com/creativejuices.paint'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FontAwesomeIcon
-              className='facebook-social'
-              icon={faSquareFacebook}
-              size='3x'
-              color='black'
-            />
-          </a>
-        </div>
-        <div>
-          <a
-            href='https://www.tiktok.com/@creativejuices_paint?_t=8Zi0xluFD8U&_r=1'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FontAwesomeIcon
-              className='tiktok-social'
-              icon={faTiktok}
-              size='3x'
-              color='black'
-            />
-          </a>
+        <div id='shellimg-contact'>
+          <img
+            id='shellimg'
+            src={shellcontact}
+          ></img>
+          <p> Thank you for supporting my small business!</p>
         </div>
       </div>
-      <div id='contact-container'>
-        <ContactForm />
+      <div className='social_contact'>
+        <div className='socials'>
+          <div>
+            <a
+              href='https://www.instagram.com/creativejuices.paint/?fbclid=IwAR1_TkmS9e-EwyQy98DVkQoBaHB21TzEQRe1wRTDB25ENU9tcapIWKZROc4'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FontAwesomeIcon
+                className='instagram-social'
+                icon={faInstagram}
+                size='3x'
+                color='black'
+              />
+            </a>
+          </div>
+          <div>
+            <a
+              href='https://www.facebook.com/creativejuices.paint'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FontAwesomeIcon
+                className='facebook-social'
+                icon={faSquareFacebook}
+                size='3x'
+                color='black'
+              />
+            </a>
+          </div>
+          <div>
+            <a
+              href='https://www.tiktok.com/@creativejuices_paint?_t=8Zi0xluFD8U&_r=1'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FontAwesomeIcon
+                className='tiktok-social'
+                icon={faTiktok}
+                size='3x'
+                color='black'
+              />
+            </a>
+          </div>
+        </div>
+        <div id='contact-container'>
+          <ContactForm />
+        </div>
       </div>
     </footer>
   );
